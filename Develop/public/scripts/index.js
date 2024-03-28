0cc65a67f6a6211b050aa66853b03df161fae642
PennyPenny76 <pengyingtong@hotmail.com>
2024-03-28 11:50:55 +1030

    finish

--- Develop/public/scripts/index.js
+++ Develop/public/scripts/index.js
@@ -123,11 +123,26 @@ const showErrors = (errorObj) => {
 
 // Helper function to send a POST request to the diagnostics route (/api/diagnostics)
 const submitDiagnostics = (submissionObj) => {
+
+  fetch('api/diagnostics', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(submissionObj),
+  })
+    .then((response) => response.json())
+    .then(() => showErrors(submissionObj.errors))
+
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+
   // TODO: your code here
-  console.info(
-    '⚠️ Create the logic for the fetch POST request in scripts/index.js'
-  );
-  alert('Add your logic to scripts/index.js');
+  // console.info(
+  //   '⚠️ Create the logic for the fetch POST request in scripts/index.js'
+  // );
+  // alert('Add your logic to scripts/index.js');
 };
 
 // Function to handle when a user submits the feedback form
--- Develop/routes/diagnostics.js
+++ Develop/routes/diagnostics.js
@@ -5,11 +5,35 @@ const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
 // GET Route for retrieving diagnostic information
 diagnostics.get('/', (req, res) => {
   // TODO: Logic for sending all the content of db/diagnostics.json
+
+  console.info(`${req.method} request received for diagnostics`);
+
+  readFromFile('./db/diagnostics.json').then((data) => res.json(JSON.parse(data)));
 });
 
 // POST Route for a error logging
 diagnostics.post('/', (req, res) => {
   // TODO: Logic for appending data to the db/diagnostics.json file
+  console.info(`${req.method} request received to submit diagnostics`);
+
+  // Destructuring assignment for the items in req.body
+  const { isValid, errors } = req.body;
+  const payload = {
+    time: Date.now(),
+    error_id: uuidv4(),
+    errors,
+  };
+
+
+    if (!isValid) {
+      readAndAppend(payload, './db/diagnostics.json');
+      res.json(`Diagnostic information added 🔧`);
+    } else {
+      res.json({
+        message: 'Object is valid, not logging. Check front end implementation',
+        error_id: payload.error_id,
+      });
+    }
 });
 
 module.exports = diagnostics;
